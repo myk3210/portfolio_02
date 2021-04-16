66855f73632053179161821070c0bd1bd711ee9b
hirose <mayu122536@gmail.com>
2021-04-16 18:28:30 +0900

    プライバシーポリシー追加をコミットし直しました。

--- js/style.js
+++ js/style.js
@@ -3,13 +3,14 @@ $(function(){
     var scrollpos;
     $('#nav-open').on('click', function(){
         scrollpos = $(window).scrollTop();
-        $('body').addClass('fixed').css({'top': -scrollpos});
+        
         $('#nav-open').addClass('open');
     });
     $('#nav-close').on('click', function(){
         $('body').removeClass('fixed').css({'top': 0});
         window.scrollTo( 0 , scrollpos );
         $('#nav-open').removeClass('open');
+        
     });
 
     $('.slider').slick({
